3ece487c1ca54ca55b8086c0cca21b6c88cb206f
nicoleweese <nweese@ymail.com>
2018-02-20 16:22:33 -0800

    solved

---
+++ lab-nicole/server.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+const cowsay = require('cowsay');
+const parseBody = require('./lib/parse-body.js');
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(function(req, res) {
+  req.url = url.parse(req.url);
+  req.url.query = querystring.parse(req.url.query);
+
+
+  if (req.method === 'GET' && req.url.pathname === '/cowsay') {
+    if (req.url.query.text === undefined) {
+      res.writeHead(400, { 'Content-Type': 'text/plain'});
+      res.write(cowsay.say({ text: 'bad request'}));
+      res.end();
+    }    
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write(cowsay.say({ text: req.url.query.text }));
+    res.end();
+    return;
+  } 
+  
+  if (req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('hello from my server!');
+    res.end();
+  }
+
+  if (req.method === 'POST' || req.method === 'PUT') {
+    console.log('req.body', req.body);
+    if (req.body === undefined) {
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
+      res.write(cowsay.say({ text: 'bad request' }));
+      res.end();
+      return;
+    }
+    parseBody(req, function (err, data) {
+      if (err) throw new Error('error');
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.write(cowsay.say({ text: data.text }));
+      res.end();
+    });
+  }
+});
+
+
+server.listen(PORT, () => {
+  console.log(`server up: ${PORT}`);
+});
